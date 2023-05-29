7f9e77e2acac9b6e77b7314c7f5eeb0a4ca3c846
VANSHIKA PAREEK <87667766+vanshikapareek@users.noreply.github.com>
2023-05-29 20:24:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+const cors = require("cors");
 import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
@@ -12,7 +13,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-const cors = require("cors");
+
 
 app.use(express.json());
 app.use(cors());
